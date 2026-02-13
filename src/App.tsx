f8eff5e9bb842c6978af02dba9cca25916f190b4
guilherme <guilherme@email.com>
2026-02-12 22:21:14 -0300

    feat: ultimo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,71 @@
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
+
 
 export function App() {
-    
-   return(
-       <>
-        <form >
-        <Input>
-        texto1
-        </Input>
-        <Button onClick={() => alert('clicado')}>
-            clique
-        </Button>
 
-        </form>
-    </>
+    const regras = z.object({
+        nome: z.string().min(1, "Incompleto"),
+        ano: z.string().min(1, "Incompleto"),
+        marca: z.string().min(1, "Incompleto"),
+        modelo: z.string().min(1, "Incompleto"),
+        cor: z.string().min(1, "Incompleto"),
+    })
+
+    type formType = z.infer<typeof regras>
+
+    const formulario = useForm<formType>({
+        resolver: zodResolver(regras)
+        
+    })
+
+    function enviarform(nome) {
+        console.log('gui')
+    }
+
+    return (
+        <>
+            <div>
+                <h1 className="bg-amber-100 w-full flex items-center justify-center">
+                    form
+                </h1>
+                <form onSubmit={formulario.handleSubmit(enviarform)} className=" flex flex-col gap-4">
+                    <div className="flex flex-col gap-4">
+                        <label htmlFor="nome" className="flex gap-2">
+                            Nome
+                            <input type="text" />
+                        </label>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <label className="flex gap-2">
+                            Ano
+                            <input type="text" />
+                        </label>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <label className="flex gap-2">
+                            Marca
+                            <input type="text" />
+                        </label>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <label className="flex gap-2">
+                            Modelo
+                            <input type="text" />
+                        </label>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <label className="flex gap-2">
+                            Cor
+                            <input type="text" />
+                        </label>
+                    </div>
+                    <button type="submit">enviar</button>
+                </form>
+            </div>
+        </>
     )
 }
